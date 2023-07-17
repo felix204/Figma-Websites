c3cf2ecf0b75cd95c254027c4ea7c3204c41e424
felix204 <icigenaydincan@gmail.com>
2023-07-17 16:59:26 +0300

    TodoList Finish1

--- Project/odev11/src/Components/TodoWrapper.jsx
+++ Project/odev11/src/Components/TodoWrapper.jsx
@@ -42,7 +42,7 @@ export const TodoWrapper = () => {
 
   return (
     <div className="TodoWrapper">
-      <h1>Get Things Done !</h1>
+      <h1>To Do List</h1>
       <TodoForm addTodo={addTodo} />
       {/* display todos */}
       {todos.map((todo) =>
